51cc080a615e4896b64eab0d05cc109eb6d53bb7
Liess INOUDJAL <liess.inoudjal@gmail.com>
2019-02-23 16:05:27 +0100

    retour arriere sur installation de webpack encore

--- config/bundles.php
+++ config/bundles.php
@@ -19,5 +19,4 @@ return [
     FOS\JsRoutingBundle\FOSJsRoutingBundle::class => ['all' => true],
     Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
     EasyCorp\Bundle\EasyAdminBundle\EasyAdminBundle::class => ['all' => true],
-    Symfony\WebpackEncoreBundle\WebpackEncoreBundle::class => ['all' => true],
 ];
